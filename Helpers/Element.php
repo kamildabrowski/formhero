663e1dab241440ac2010c2b37ecbac930b641733
Kamil Dabrowski <kamildabrowski@users.noreply.github.com>
2024-11-19 02:59:16 +0100

    Delete Helpers directory

--- Helpers/Element.php
+++
@@ -1,4 +0,0 @@
-<?php namespace FormHero\Helpers;
-interface Element {
-
-}
--- Helpers/View.php
+++
@@ -1,9 +0,0 @@
-<?php namespace FormHero\Helpers;
-
-use FormHero\Helpers\Element as ElementHelper;
-
-class View {
-    public function __construct(public ElementHelper $elementHelper) {
-
-    }
-}
